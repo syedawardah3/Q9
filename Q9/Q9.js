37df3ff3297ac88fc17f8a51821502c023936c24
syedawardah3 <148571080+syedawardah3@users.noreply.github.com>
2024-02-09 18:40:57 +0500

    Add files via upload

---
+++ Q9/Q9.js
@@ -0,0 +1,4 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 7;
+var message = "My favorite number is ".concat(favoriteNumber, ".");
+console.log(message);
